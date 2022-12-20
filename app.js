a6f7163a6561819c1c2f86740bb7bd987237306e
sd880428 <sd660428@gmail.com>
2022-12-21 01:03:57 +0800

    add shorted page

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const PORT = process.env.PORT || 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.get ('/', (req,res) => {
+app.get ('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/shorted', (req, res) => {
+  res.render('shorted')
 })
 
 app.listen(PORT, () => {
