694f6a9dbcfb00e18af965e3c5b868b9d777e0bd
sd880428 <sd660428@gmail.com>
2022-12-21 01:11:33 +0800

    改用express內建的bodyparser

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const PORT = process.env.PORT || 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get ('/', (req, res) => {
   res.render('index')
 })
 
 app.post('/shorted', (req, res) => {
+  console.log(req.body.url)
   res.render('shorted')
 })
 
