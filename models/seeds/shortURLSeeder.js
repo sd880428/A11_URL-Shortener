b1b845333f9b1e3c1d702708ff8dfb719028ff8f
sd880428 <sd660428@gmail.com>
2022-12-21 23:21:00 +0800

    add more seed

--- models/seeds/shortURLSeeder.js
+++ models/seeds/shortURLSeeder.js
@@ -5,6 +5,10 @@ db.once('open', () => {
   shortURL.create({
     URLId: 'Ab123',
     originURL: 'https://www.google.com.tw/?gws_rd=ssl'
+  },
+  {
+    URLId: '123AB',
+    originURL: 'https://www.youtube.com/'
   })
 
   console.log('seeder has created!')
