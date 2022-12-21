02e3d056cc88aabf516d6121231773aa0a456013
sd880428 <sd660428@gmail.com>
2022-12-21 22:32:57 +0800

    重購mongoose路由

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const routes = require('./routes')
 const exphbs = require('express-handlebars')
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+require('./config/mongoose')
 
 const app = express() 
 const PORT = process.env.PORT || 3000
